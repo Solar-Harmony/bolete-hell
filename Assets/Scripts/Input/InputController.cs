4e0fa13e9cbd5c75ba26afb1747474014e56ae85
Thurinum <m.gagnon941@gmail.com>
2025-05-02 20:17:37 -0400

    feat: Add simple player movement, cinemachine camera, and tileset based world

---
+++ Assets/Scripts/Input/InputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Input
+{
+    public class InputController : MonoBehaviour
+    {
+        private InputSystem_Actions _actions;
+        
+        private void Awake()
+        {
+            _actions = new InputSystem_Actions();
+            _actions.Enable();
+        }
+        
+        public Vector2 GetMovementDisplacement()
+        {
+            return _actions.Player.Move.ReadValue<Vector2>();
+        }
+    }
+}
---
+++ Assets/Scripts/Input/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utils;
+
+namespace Input
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private InputController input;
+        
+        public float speed = 5f;
+
+        private void Start()
+        {
+            this.AssertNotNull(input);
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector2 delta = input.GetMovementDisplacement();
+            transform.Translate(delta * speed * Time.deltaTime);
+        }
+    }
+}
