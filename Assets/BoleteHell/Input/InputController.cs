eb9b0e7b344e2fbe67401249fc8e73810a5ab47b
unknown <danthelyon99>
2025-05-05 21:20:31 -0400

    added dodging mechanic with spacebar

--- Assets/BoleteHell/Input/InputController.cs
+++ Assets/BoleteHell/Input/InputController.cs
@@ -5,20 +5,22 @@ namespace Input
     public class InputController : MonoBehaviour
     {
         private InputSystem_Actions _actions;
-        
+
         private void Awake()
         {
             _actions = new InputSystem_Actions();
             _actions.Enable();
         }
-        
+
         public Vector2 GetMovementDisplacement()
         {
             return _actions.Player.Move.ReadValue<Vector2>();
         }
-        
+
         public bool IsMoving => GetMovementDisplacement() != Vector2.zero;
         public bool IsShooting => _actions.Player.Attack.triggered;
         public bool IsBoosting => _actions.Player.Sprint.triggered;
+
+        public bool isDodging => _actions.Player.Dodge.IsPressed();
     }
 }
--- Assets/BoleteHell/Input/PlayerMovement.cs
+++ Assets/BoleteHell/Input/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using BoleteHell.Graphics;
 using BoleteHell.Utils;
 using Input;
@@ -16,10 +17,10 @@ namespace BoleteHell.Input
 
         [SerializeField]
         private int enemyDamage = 5;
-        
+
         [SerializeField]
         private int health = 100;
-        
+
         [SerializeField]
         private SpriteRuntimeFragmenter fragmenter;
 
@@ -28,21 +29,48 @@ namespace BoleteHell.Input
 
         [field: SerializeField]
         public float SpeedFactor { get; private set; } = 1.0f;
-        
+
         [SerializeField]
         private Light2D shipExhaustLight;
-        
+
         [field: SerializeField]
         private float maxLightIntensity = 5.0f;
-        
+
+        [SerializeField]
+        private float dodgeSpeed = 20f;
+
+        [SerializeField]
+        private float dodgeDuration = 0.2f;
+
+        [SerializeField]
+        private float invincibilityDuration = 2f;
+
+        [SerializeField]
+        private float stutterInterval = 0.1f;
+
+        [SerializeField]
+        private int stutterCount = 4;
+
+        [SerializeField]
+        private float stutterLifetime = 0.5f;
+
+        [SerializeField]
+        private Color stutterColor = new Color(1f, 1f, 1f, 0.3f);
+
+        private bool canDodge = true;
+        private bool isInvincible = false;
+        [SerializeField] SpriteRenderer spriteRenderer;
+
+
         private void OnGUI()
         {
             GUI.skin.label.fontSize = 32;
             GUI.Label(new Rect(10, 10, 300, 80), "Health: " + health);
         }
-        
+
         private void FixedUpdate()
         {
+            Dodge();
             bool isBoosting = Keyboard.current.shiftKey.isPressed;
 
             shipExhaustLight.intensity = isBoosting ? maxLightIntensity / 2.0f : maxLightIntensity;
@@ -60,13 +88,76 @@ namespace BoleteHell.Input
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.CompareTag("Enemy"))
+            if (other.gameObject.CompareTag("Enemy") && !isInvincible)
             {
                 if (health <= 0)
                     fragmenter.Fragment(other.GetContact(0).normal);
 
                 health -= enemyDamage;
             }
+        }
+
+        private void Dodge()
+        {
+            if (input.isDodging && canDodge)
+            {
+                StartCoroutine(dodgeRoutine());
+            }
+        }
+
+        private IEnumerator dodgeRoutine()
+        {
+            canDodge = false;
+            isInvincible = true;
+            
+            // Get the movement direction
+            Vector2 moveDir = input.GetMovementDisplacement().normalized;
+            
+            // If no movement input, don't dodge
+            if (moveDir == Vector2.zero)
+            {
+                canDodge = true;
+                isInvincible = false;
+                yield break;
+            }
+            
+            // Store original position for stutter effect
+            Vector3 originalPos = transform.position;
+            
+            // Perform the dodge movement
+            float elapsedTime = 0f;
+            while (elapsedTime < dodgeDuration)
+            {
+                transform.position += (Vector3)moveDir * (dodgeSpeed * Time.deltaTime);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            
+            // Create stutter effect
+            for (int i = 0; i < stutterCount; i++)
+            {
+                // Create a visual copy of the player
+                GameObject stutterCopy = new GameObject("DodgeStutter");
+                stutterCopy.transform.position = transform.position;
+                stutterCopy.transform.rotation = transform.rotation;
+                SpriteRenderer stutterSprite = stutterCopy.AddComponent<SpriteRenderer>();
+                stutterSprite.sprite = spriteRenderer.sprite;
+                stutterSprite.color = stutterColor;
+                stutterSprite.sortingOrder = spriteRenderer.sortingOrder - 1;
+                
+                // Destroy the copy after the specified lifetime
+                Destroy(stutterCopy, stutterLifetime);
+                
+                yield return new WaitForSeconds(stutterInterval);
+            }
+            
+            // Wait for invincibility to end
+            yield return new WaitForSeconds(invincibilityDuration - dodgeDuration);
+            isInvincible = false;
+            
+            // Wait for remaining cooldown
+            yield return new WaitForSeconds(5f - invincibilityDuration);
+            canDodge = true;
         }
     }
 }
