4eea00d243b4820e83f4199c40135f1e351facfc
Thurinum <m.gagnon941@gmail.com>
2025-05-13 21:18:48 -0400

    feat: Spline-based terrain framework

---
+++ Assets/BoleteHell/Utils/LightFollowCursor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+
+namespace Utils
+{
+    // Useful class for testing lighting when LD
+    public class LightFollowCursor : MonoBehaviour
+    {
+        [SerializeField]
+        private Light2D _light;
+        
+        [SerializeField]
+        private Camera _mainCamera;
+
+        private void Update()
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Vector3 worldPos = _mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, _mainCamera.nearClipPlane));
+            _light.transform.position = new Vector3(worldPos.x, worldPos.y, _light.transform.position.z);
+            
+            float scroll = Mouse.current.scroll.ReadValue().y;
+            if (scroll != 0)
+            {
+                _light.pointLightOuterRadius += scroll * Time.deltaTime;
+                _light.pointLightInnerRadius += scroll * Time.deltaTime;
+            }
+        }
+    }
+}
