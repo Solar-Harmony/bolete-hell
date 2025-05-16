e51ee0efb369a11edf55cac59b12f7f5a772ee48
Thurinum <m.gagnon941@gmail.com>
2025-05-16 19:40:29 -0400

    feat: Improve debug light (press V)

--- Assets/BoleteHell/Utils/LightFollowCursor.cs
+++ Assets/BoleteHell/Utils/LightFollowCursor.cs
@@ -12,9 +12,17 @@ namespace Utils
         
         [SerializeField]
         private Camera _mainCamera;
-
+        
         private void Update()
         {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.V))
+            {
+                _light.enabled = !_light.enabled;
+            }
+            
+            if (!_light.enabled)
+                return;
+            
             Vector2 mousePos = Mouse.current.position.ReadValue();
             Vector3 worldPos = _mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, _mainCamera.nearClipPlane));
             _light.transform.position = new Vector3(worldPos.x, worldPos.y, _light.transform.position.z);
@@ -23,7 +31,6 @@ namespace Utils
             if (scroll != 0)
             {
                 _light.pointLightOuterRadius += scroll * Time.deltaTime;
-                _light.pointLightInnerRadius += scroll * Time.deltaTime;
             }
         }
     }
