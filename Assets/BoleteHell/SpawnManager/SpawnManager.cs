56b42e22e782d241e5060b2f697645a51e3d7ff7
Atlearia <ronanthenickname@gmail.com>
2025-05-23 09:01:23 -0400

    spawner system: have not added elite, biome, or difficulty scaling yet

---
+++ Assets/BoleteHell/SpawnManager/SpawnManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public SpawnList spawnList;  //just a spawnlist of biome for now i havent defined how to collide with biome logic
+    public SpawnPoint spawnPoints;
+    public float weightGainPerSecond = 1f;
+    private float currentWeight = 0f;
+    private EnemyData targetEnemy;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCycle());
+    }
+
+    private IEnumerator SpawnCycle()
+    {
+        while (true)
+        {
+            // gamba choose enemy and determine weight
+            var entries = spawnList.allowedEnemies;
+            if (entries == null || entries.Length == 0)
+                yield break;
+
+            targetEnemy = entries[Random.Range(0, entries.Length)]; //gamba enemy
+            float cost = targetEnemy.spawnWeight; //find weight
+
+            // Accumulate weight until we can afford it / we can add a thing where spawner gets more weight with time
+            while (currentWeight < cost)
+            {
+                currentWeight += weightGainPerSecond * Time.deltaTime;
+                yield return null;
+            }
+
+            // spawn the target enemy
+            SpawnSelectedEnemy(targetEnemy);
+
+            // Pay the money lol
+            currentWeight -= cost;
+        }
+    }
+    public void SpawnSelectedEnemy(EnemyData targetEnemy)
+    {
+        // call spawnpoint.cs for help pls uwu
+        Vector3 spawnPos = spawnPoints.GetSpawnPosition(targetEnemy);
+
+        Instantiate(targetEnemy, spawnPos, Quaternion.identity);
+    }
+}
