7e34426f821380a8c5e2d0fd6bacdcfb4692e3bc
Atlearia <ronanthenickname@gmail.com>
2025-06-05 00:28:44 +0200

    changeX

--- Assets/BoleteHell/SpawnManager/SpawnManager.cs
+++ Assets/BoleteHell/SpawnManager/SpawnManager.cs
@@ -3,32 +3,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    public bool Spawn(SpawnArea spawnArea, Transform spawnPoint)
+    public bool Spawn(SpawnArea spawnArea)
     {
         var entries = spawnArea.spawnList.allowedEnemies;
         if (entries == null || entries.Length == 0)
             return false;
 
-        SpawnSelectedEnemy(spawnArea.spawnList, spawnPoint, spawnArea);
+        SpawnSelectedEnemy(spawnArea.spawnList, spawnArea);
         return true;
     }
 
-    public Vector2 GetSpawnPosition(SpawnArea spawnArea, Transform spawnPoint)
+    public Vector2 GetSpawnPosition(SpawnArea spawnArea)
     {
         Vector2 dir2D = Random.insideUnitCircle.normalized;
-
-        float dist = Random.Range(spawnArea.minSpawnRadius,spawnArea.maxSpawnRadius);
-
-        Vector2 offset2D = dir2D * dist;
-        Vector2 center2D = new Vector2(spawnPoint.position.x, spawnPoint.position.y);
-
-        Vector2 fml = new Vector2(center2D.x + offset2D.x,center2D.y + offset2D.y);
-        return fml;
+        float dist = Random.Range(spawnArea.minSpawnRadius, spawnArea.maxSpawnRadius);
+        Vector2 center2D = new Vector2(spawnArea.transform.position.x, spawnArea.transform.position.y);
+        return center2D + dir2D * dist;
     }
 
-    public void SpawnSelectedEnemy(SpawnList allowedEnemies, Transform spawnPoint, SpawnArea spawnArea)
+    public void SpawnSelectedEnemy(SpawnList allowedEnemies, SpawnArea spawnArea)
     {
-        Vector3 finalSpawnPos = GetSpawnPosition(spawnArea, spawnPoint);
+        Vector3 finalSpawnPos = GetSpawnPosition(spawnArea);
         int index = Random.Range(0, allowedEnemies.allowedEnemies.Length);
         GameObject prefabToSpawn = allowedEnemies.allowedEnemies[index];
 
