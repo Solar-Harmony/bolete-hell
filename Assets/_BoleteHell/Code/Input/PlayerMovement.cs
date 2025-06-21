c995b7f6d4b5db4d9f53bac46370c5c170e73e44
Thurinum <m.gagnon941@gmail.com>
2025-06-21 11:00:26 -0400

    feat(player): Feed velocity to kinematic rigid body; fix player destruction fragments impulse

--- Assets/_BoleteHell/Code/Input/PlayerMovement.cs
+++ Assets/_BoleteHell/Code/Input/PlayerMovement.cs
@@ -1,10 +1,12 @@
-﻿using Graphics;
+﻿using System;
+using Graphics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 
 namespace Input
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private InputController input;
@@ -15,19 +17,29 @@ namespace Input
 
         [field: SerializeField] private float maxLightIntensity = 5.0f;
 
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
         private void FixedUpdate()
         {
             shipExhaustLight.intensity = input.IsBoosting ? maxLightIntensity / 2.0f : maxLightIntensity;
 
             var inputDir = input.GetMovementDisplacement().normalized;
             var speed = input.IsBoosting ? 2.0f * SpeedFactor : SpeedFactor;
-            transform.position += (Vector3)inputDir * (speed * Time.fixedDeltaTime);
+            Vector2 newPosition = transform.position + (Vector3)inputDir * (speed * Time.fixedDeltaTime);
 
             var mousePos = input.MousePosition;
             var screenCenter = new Vector2(Screen.width, Screen.height) * 0.5f;
             var lookDir = (mousePos - screenCenter).normalized;
             var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            Quaternion newRotation = Quaternion.Euler(0, 0, angle);
+            
+            _rb.MovePositionAndRotation(newPosition, newRotation);
+            _rb.linearVelocity = inputDir * speed;
         }
     }
 }
