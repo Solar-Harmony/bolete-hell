9dd523027745db9007a91164b62e9f61347c1bef
Thurinum <m.gagnon941@gmail.com>
2025-06-25 22:31:59 -0400

    chore: Add Zenject dependency injection framework

---
+++ Assets/BoleteHell/Code/GameInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace BoleteHell.Code
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
