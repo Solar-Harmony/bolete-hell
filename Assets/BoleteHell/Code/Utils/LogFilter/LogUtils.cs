e064173a600699eb6aa3fc4fd0d0761367dfc005
Thurinum <m.gagnon941@gmail.com>
2025-11-19 21:22:18 -0500

    feat: Support varargs format when logging

--- Assets/BoleteHell/Code/Utils/LogFilter/LogUtils.cs
+++ Assets/BoleteHell/Code/Utils/LogFilter/LogUtils.cs
@@ -7,7 +7,7 @@ namespace BoleteHell.Code.Utils.LogFilter
     public static class Scribe
     {
         [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-        public static void Log(LogType type, LogCategory cat, string message)
+        public static void Log(LogType type, LogCategory cat, string message, params object[] args)
         {
             var category = LogCategory.GetCategoriesDictionary()[cat.Name];
             if (category is { Enabled: false }) 
@@ -16,25 +16,25 @@ namespace BoleteHell.Code.Utils.LogFilter
             Color32 c = category.Color;
             string colorHex = $"#{c.r:X2}{c.g:X2}{c.b:X2}{c.a:X2}";
             var format = $"<b><color={colorHex}>[{category.Name}]</color></b> {message}";
-            Debug.LogFormat(type, LogOption.None, null, format);
+            Debug.LogFormat(type, LogOption.None, null, format, args);
         }
         
         [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-        public static void Log(LogCategory category, string message)
+        public static void Log(LogCategory category, string message, params object[] args)
         {
-            Log(LogType.Log, category, message);
+            Log(LogType.Log, category, message, args);
         }
 
         [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-        public static void LogWarning(LogCategory category, string message)
+        public static void LogWarning(LogCategory category, string message, params object[] args)
         {
-            Log(LogType.Warning, category, message);
+            Log(LogType.Warning, category, message, args);
         }
 
         [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
-        public static void LogError(LogCategory category, string message)
+        public static void LogError(LogCategory category, string message, params object[] args)
         {
-            Log(LogType.Error, category, message);
+            Log(LogType.Error, category, message, args);
         }
     }
 }
