10d5c098595791b56f4043e2468d39faf5c534ee
Thurinum <m.gagnon941@gmail.com>
2025-12-05 16:26:50 -0500

    feat: Tung sahur

---
+++ Assets/BoleteHell/Rendering/VFX/FakeShadow.cs
@@ -0,0 +1,27 @@
+﻿using BoleteHell.Utils;
+using UnityEngine;
+
+namespace BoleteHell.Rendering.VFX
+{
+    public class FakeShadow : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _shadowObject;
+
+        // TODO: Synchronize with render feature settings
+        [SerializeField]
+        [AnglePicker]
+        private Vector2 _shadowDirection = new(0.1f, 0.1f);
+
+        private void Awake()
+        {
+            Debug.Assert(_shadowObject);
+        }
+
+        private void Update()
+        {
+            Vector3 shadowOffset = new Vector3(_shadowDirection.x, 0.0f, _shadowDirection.y).normalized * 0.1f;
+            _shadowObject.transform.position = shadowOffset;
+        }
+    }
+}
